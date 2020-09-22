ce2d2b970302872c45a87af6b2cd9baeb2e4f062
Josh Bone <jbone@bu.edu>
2020-09-22 17:36:09 -0400

    Update HABBOF_GtToJSON.py

--- HABBOF_GtToJSON.py
+++ HABBOF_GtToJSON.py
@@ -12,21 +12,33 @@ paths = ["Lab1", "Lab2", "Meeting1", "Meeting2"]
 
 for p in paths:
     datalist = []
+    imlist = []
     dumpfile = HABBOF_path + "annotations/" + p + ".json"
     for fname in os.listdir(HABBOF_path + p):
         full_file = HABBOF_path + p + "/" + fname
         if(fname[-3:] == "txt"):
+            imdata = {}
+            imdata["file_name"] = fname.replace(".txt", ".jpg")
+            imdata["id"] = fname.replace(".txt", "")
+            imdata["width"] = 2048
+            imdata["height"] = 2048
+            imlist.append(imdata)
             with open(full_file, "r") as f:
                 lines = f.read().splitlines()
                 for detection in lines:
                     data = {}
-                    box = [int(i) for i in detection.split()[1:]]
+                    box = [float(i) for i in detection.split()[1:]]
                     data["image_id"] = fname.replace(".txt", "")
+                    data["category_id"] = 1
+                    data["iscrowd"] = 0
+                    data["area"] = box[2] * box[3]
                     data["bbox"] = box
-                    data["score"] = 1  # meaningless
-                    data["segmentation"] = []  # meaningless
+                    data["segmentation"] = []
                     datalist.append(data)
     dump_data = {}
     dump_data["annotations"] = datalist
+    dump_data["images"] = imlist
+    dump_data["categories"] = [
+        {"id": 1, "name": "person", "supercategory": "person"}]
     with open(dumpfile, 'w') as f:
         json.dump(dump_data, f, indent=1)
